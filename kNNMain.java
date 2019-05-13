0d08d49a91ea816bfcc68b2cde728573b0dda6ba
BarnesElizabeth <senrab.htebazile@gmail.com>
2019-05-13 11:25:43 -0400

    1000 repetitions with precision and accuracy

--- kNNMain.java
+++ kNNMain.java
@@ -7,93 +7,123 @@ import java.util.Scanner;
 
 public class kNNMain{
 
-  public static void main(String... args) throws FileNotFoundException{
-
-     // TASK 1: Use command line arguments to point DataSet.readDataSet method to
-    // the desired file. Choose a given DataPoint, and print its features and label
-
-	List <DataPoint> allTheData = DataSet.readDataSet(args[0]);
-		
-	String label = allTheData.get(0).getLabel();
-	double[] X = allTheData.get(0).getX();	
-			
-	for(int i=0; i<X.length; i++){System.out.print(X[i] +"\t");}
-	System.out.println(label);
-			
-	double train = 0.2; // the % of data to reserve for later
-
-    //TASK 2:Use the DataSet class to split the fullDataSet into Training and Held Out Test Dataset
-	List<DataPoint> testData = DataSet.getTestSet(allTheData, train); 
-	String testLabel = testData.get(0).getLabel();
-	double[] testX = testData.get(0).getX();	
-			
-	for(int i=0; i<testX.length; i++){System.out.print(testX[i] +"\t");}
-	System.out.println(testLabel);
-
-	List<DataPoint> trainData = DataSet.getTrainingSet(allTheData, (1-train)); //will train with ohter set of data
-	String trainLabel = trainData.get(0).getLabel();
-	double[] trainX = trainData.get(0).getX();	
-			
-	for(int i=0; i<trainX.length; i++){System.out.print(trainX[i] +"\t");}
-	System.out.println(trainLabel);
-
-
-    // TASK 3: Use the DataSet class methods to plot the 2D data (binary and multi-class)
-
-
-
-    // TASK 4: write a new method in DataSet.java which takes as arguments to DataPoint objects,
-    // and returns the Euclidean distance between those two points (as a double)
-	DataPoint first = trainData.get(0);
-	DataPoint second = trainData.get(1);
-	double d = DataSet.distanceEuclid(first, second);
+	public static void main(String... args) throws FileNotFoundException{
+	int repetitions = 1000;
 	
-	System.out.println(d);
-
-
-    // TASK 5: Use the KNNClassifier class to determine the k nearest neighbors to a given DataPoint,
-    // and make a print a predicted target label
-	KNNClassifier Classifier3 = new KNNClassifier(3);
 	
-	DataPoint[] NeighbourDistances = Classifier3.getNearestNeighbors(trainData, trainData.get(0));
-	
-	String prediction = Classifier3.predict(trainData, trainData.get(0));
-	String answer = testData.get(0).getLabel();
-
-	System.out.println(prediction);
-
-    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
-    // point based on nearest neighbors in training set. Calculate accuracy of model.
-
 	int k = 5;
 	Scanner keyboard = new Scanner(System.in);
 	System.out.print("k: ");
 	
 	k = keyboard.nextInt();
 	
+
+	double[] accuracy = new double[repetitions];
+	double[] precision = new double[repetitions];
+	double[] recall = new double[repetitions];
 	
+	for(int r=0; r<repetitions; r++){
+
+    // TASK 1: Point DataSet.readDataSet method to the desired file. 
+	List <DataPoint> allTheData = DataSet.readDataSet(args[0]);
+		
+	String label = allTheData.get(0).getLabel();
+	double[] X = allTheData.get(0).getX();	
+		
+	double train = 0.3; // the % of data to reserve for later
+	
+    // TASK 2: Split the fullDataSet into Training and Held Out Test Dataset
+	List<DataPoint> testData = DataSet.getTestSet(allTheData, train); 
+	
+	String testLabel = testData.get(0).getLabel();
+	double[] testX = testData.get(0).getX();	
+		
+	List<DataPoint> trainData = DataSet.getTrainingSet(allTheData, (1-train)); //will train with ohter set of data
+	
+	String trainLabel = trainData.get(0).getLabel();
+	double[] trainX = trainData.get(0).getX();	
+			
+ 
+	
+
+	
+    // TASK 6: loop over the datapoints in the held out test set, and make predictions for Each
+    // point based on nearest neighbors in training set. Calculate accuracy of model.
+	
+
+
 	KNNClassifier myNewClassifier = new KNNClassifier(k);
+
+	String prediction = myNewClassifier.predict(trainData, testData.get(0));
+	String answer = testData.get(0).getLabel();
 	
 	int i = 0;
-	int correct = 0;
+	
+
+	
+	int benignBenign = 0;			// predicted as benign, actually benign			(true negative)
+	int benignMalignant = 0;		// predicted as benign, actually malignant		(false negative)
+	int malignantBenign = 0;		// predicted as malignant, actually benign		(false positive)
+	int malignantMalignant = 0;		// predicted as malignant, actually malignant	(true positive)
+	
+	
 		for(i = 0; i<testData.size(); i++){
 			prediction = myNewClassifier.predict(trainData, testData.get(i));
 			answer = trainData.get(i).getLabel();
 			
-			System.out.print("prediction: " + prediction);
-			System.out.print("\t\t\tanswer: " + answer);
+//			System.out.print("prediction: " + prediction);
+//			System.out.print("\t\t\tanswer: " + answer);
+			
+			// benign has length 6
+			// malignant has length 9
 			
 			if(prediction.length() == answer.length()){ // check lengths because checking values did not work
-				correct++;
-				System.out.println("\t\t\tYES");
+//				System.out.println("\t\t\tYES");
+
+				if(answer.length() == "benign".length()){
+					benignBenign ++;
+				}else{
+					malignantMalignant ++;
+				}
+				
 			}else{
-				System.out.println("\t\t\tNO");
+//				System.out.println("\t\t\tNO");
+
+				if(answer.length() == "benign".length()){
+					malignantBenign ++;
+				}else{
+					benignMalignant ++;
+				}
+
 			}
 		
 		}
 		
-		System.out.println(correct + " out of " + i);
-  }
+	int correct = benignBenign + malignantMalignant;
+	int actuallyMalignant = malignantMalignant + benignMalignant;
+	int predictedMalignant = malignantMalignant + malignantBenign;
+		
+		
+		
+		accuracy[r] = (correct *100/i);
+		precision[r] = (malignantMalignant*100/predictedMalignant);
+		recall[r] =  (malignantMalignant*100/actuallyMalignant);
+		
+		
+//		System.out.println(correct + " out of " + i);
+  
+	} // 1000 repetitions
+	
+	
+	for(int i=0; i<accuracy.length; i++){
+		System.out.print(accuracy[i] + ", ");
+	}
+		
+	System.out.println("\nACCURACY\nmean: " + mean(accuracy) + "\nstandard deviation: " + standardDeviation(accuracy));
+	System.out.println("\nPRECISION\nmean: " + mean(precision) + "\nstandard deviation: " + standardDeviation(precision));
+	System.out.println("\nRECALL\nmean: " + mean(recall) + "\nstandard deviation: " + standardDeviation(recall));
+	
+  } // main
 
   public static double mean(double[] arr){
     /*
